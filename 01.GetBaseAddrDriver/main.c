1c790a8f8a53eabc47052315cc83c98d4b82d406
LethalSnake <lethalsnake@outlook.com>
2023-01-25 13:04:58 +0100

    Device and SymLink Creation

--- 01.GetBaseAddrDriver/main.c
+++ 01.GetBaseAddrDriver/main.c
@@ -2,13 +2,20 @@
 
 #define DRIVER_DEV_DOS = L"\\\\DosDevice\\DriverDev"
 
+void Unload(_In_ PDRIVER_OBJECT DriverObject) {
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\DriverDev");
+	IoDeleteSymbolicLink(&symLink);
+
+	IoDeleteDevice(DriverObject->DeviceObject);
+}
+
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
 	UNREFERENCED_PARAMETER(RegistryPath);
 
 	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\DriverDev");
 	PDEVICE_OBJECT DeviceObject;
-	IoCreateDevice(
+	NTSTATUS status = IoCreateDevice(
 		DriverObject,
 		0,
 		&devName,
@@ -16,6 +23,16 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
 		0,
 		FALSE,
 		&DeviceObject);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create device object (0x%08X)\n", status));
+		return status;
+	}
 
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\DriverDev");
+	status = IoCreateSymbolicLink(&symLink, &devName);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
+		IoDeleteDevice(DeviceObject);
+	}
 	return STATUS_SUCCESS;
 }
