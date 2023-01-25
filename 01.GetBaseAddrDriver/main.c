ad11e795e698fc82461510f64b47194375267f40
LethalSnake <lethalsnake@outlook.com>
2023-01-25 11:34:18 +0100

    CreateDevice

--- 01.GetBaseAddrDriver/main.c
+++ 01.GetBaseAddrDriver/main.c
@@ -1,11 +1,21 @@
 #include <ntddk.h>
 
+#define DRIVER_DEV_DOS = L"\\\\DosDevice\\DriverDev"
 
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
-	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
 
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\DriverDev");
+	PDEVICE_OBJECT DeviceObject;
+	IoCreateDevice(
+		DriverObject,
+		0,
+		&devName,
+		FILE_DEVICE_UNKNOWN,
+		0,
+		FALSE,
+		&DeviceObject);
 
 	return STATUS_SUCCESS;
 }
